aab375d481f743f49b83a40732210662a5aae68b
milosstoiljkovic <milos.sto82@gmail.com>
2025-12-20 20:14:37 +0100

    extracting name and email

--- merchant/src/main/java/com/pk/commerce/merchant/rest/merchant/MerchantController.java
+++ merchant/src/main/java/com/pk/commerce/merchant/rest/merchant/MerchantController.java
@@ -1,6 +1,8 @@
 package com.pk.commerce.merchant.rest.merchant;
 
 import com.pk.commerce.merchant.api.RestResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,9 +16,16 @@ public class MerchantController {
     //we should get redirected here from login.
     @GetMapping("/welcome")
     @ResponseBody
-    RestResponse welcomePage(){
+    RestResponse welcomePage(@AuthenticationPrincipal OAuth2User user) {
         //display merchant info and status.
         //depending on status, merchant actions will be enabled
-        return new RestResponse("", Optional.empty());
+        String email = user.getAttribute("email");
+        String name = user.getAttribute("name");
+        String picture = user.getAttribute("picture");
+
+        // if email exists and registration pending, show current info
+        // else display registration details form
+
+        return new RestResponse(String.format("name: %s, email: %s", name, email), Optional.empty());
     }
 }
